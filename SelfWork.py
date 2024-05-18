54baef215a66c27c07d02262750558a3289e5ef2
SSSh18 <168445136+SSSh18@users.noreply.github.com>
2024-05-18 10:51:35 +0300

    Add files via upload

---
+++ SelfWork.py
@@ -0,0 +1,7 @@
+def print_params(colour): #Создайте функцию def print_params, которая в своем теле будет распечатывать переданное значение из параметра 2 раза
+    print ("This is", colour)
+print_params('White' * 2)
+
+print_params('Red') #Вызовите данную функцию несколько раз в том же файле
+print_params('Blue')
+print_params('Green')
