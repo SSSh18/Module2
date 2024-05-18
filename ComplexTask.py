65a78a07a526aacdf1730102d0d69cc8f60b70ca
SSSh18 <168445136+SSSh18@users.noreply.github.com>
2024-05-18 18:54:38 +0300

    Add files via upload

---
+++ ComplexTask.py
@@ -0,0 +1,16 @@
+def generate_password(n):
+    if n < 3 or n > 20:
+        return "Число должно быть от 3 до 20"
+
+    result = ""
+
+    for i in range(1, n):
+        for j in range(i, n):
+            if (i + j) % n == 0 and i != j:
+                result += str(i) + str(j)
+
+    return result
+
+
+n = int(input("Введите число от 3 до 20: "))
+print(generate_password(n))
